d95e073cd7d15c6d0db130ad96ce87a57c2aff40
Andrew Rodriguez <andrewrb22@gmail.com>
2022-04-28 22:07:16 -0400

    new questions, kepp testing

--- practiceFinal.c
+++ practiceFinal.c
@@ -3,6 +3,15 @@
 
 int main(){
 
+int counter = 0;
+for ( counter = 2 ; counter <10; counter++)
+{
+i f ( counter <5 ) continue ;
+e l s e i f ( counter >=6 ) break ;
+p r i n t f (” Inside Loop ” ) ;
+counter ++;
+}
+p r i n t f (” Outside Loop ” ) ;
 
 return 0;
 
